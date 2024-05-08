560e60e20ced86adf1b13e8cd78a3ce373d1f0c4
GohanVu <hoangvhdata@gmail.com>
2024-05-08 13:59:36 +0700

    create repo 2

---
+++ ss1_Java Introduction/src/Practice/hien_thi_thoi_gian_he_thong_v2023.java
@@ -0,0 +1,10 @@
+package Practice;
+
+import java.util.Date;
+
+public class hien_thi_thoi_gian_he_thong_v2023 {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: "+now);
+    }
+}
