2573b8d2b4a240f959be633e0dd69d10752a05e8
GohanVu <hoangvhdata@gmail.com>
2024-05-09 16:05:47 +0700

    update practies

---
+++ ss2_Loop/Practice/src/tim_uoc_so_chung_lon_nhat_v2023.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class tim_uoc_so_chung_lon_nhat_v2023 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = scanner.nextInt();
+        System.out.println("Enter b: ");
+        b = scanner.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0) {
+            System.out.println("No greatest common factor");
+        }
+        while ( a!=b){
+            if (a > b){
+                a = a - b;
+            } else {
+                b = b - a ;
+            }
+        }
+        System.out.println("Greatest common factor: "+a);
+    }
+}
