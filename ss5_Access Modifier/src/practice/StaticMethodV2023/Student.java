51a9ffa577728960656cd0b0c6822eb431f65b8a
GohanVu <hoangvhdata@gmail.com>
2024-05-14 15:54:41 +0700

    update practice ss5

---
+++ ss5_Access Modifier/src/practice/StaticMethodV2023/Student.java
@@ -0,0 +1,18 @@
+package practice.StaticMethodV2023;
+
+public class Student {
+    private int rollno;
+    private String name;
+    private  static  String college = "LGBT";
+
+    Student (int r, String n){
+        rollno = r;
+        name = n ;
+    }
+    static  void change(){
+        college = "Codegym";
+    }
+    void  display(){
+        System.out.println(rollno+" "+name+ " "+ college);
+    }
+}
