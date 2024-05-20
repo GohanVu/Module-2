0460a13f83852d2ed1cf5be11816a94059ea55d7
GohanVu <hoangvhdata@gmail.com>
2024-05-20 09:35:48 +0700

    update ss9 practice

---
+++ ss9_TDD/src/practice/FindAbsoluteValue/AbsoluteNumberCalculator.java
@@ -0,0 +1,10 @@
+package practice.FindAbsoluteValue;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+        if(number<0){
+            return -number;
+        }
+        return number;
+    }
+}
---
+++ ss9_TDD/src/practice/FindAbsoluteValue/AbsoluteNumberCalculatorTest.java
@@ -0,0 +1,26 @@
+package practice.FindAbsoluteValue;
+import  org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class AbsoluteNumberCalculatorTest {
+    @Test
+    void testFindAbsolute0(){
+        int number = 0;
+        int expected = 0;
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected,result);
+    }
+    @Test
+    void testFindAbsolute(){
+        int number = 1;
+        int expected = 1;
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected,result);
+    }
+    @Test
+    void testFindAbsoluteNegative1(){
+        int number = -1;
+        int excepted = 1;
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(excepted,result);
+    }
+}
