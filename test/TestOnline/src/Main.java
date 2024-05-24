7d16881a35bcec7f313d25fe2ea816d0ec5246a5
GohanVu <hoangvhdata@gmail.com>
2024-05-24 08:31:48 +0700

    update practice ss12

--- test/TestOnline/src/Main.java
+++ test/TestOnline/src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Enter large of array: ");
+        Scanner sc = new Scanner(System.in);
+        int size = Integer.parseInt(sc.nextLine());
+        int [] arrays = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter value "+ (i+1)+": ");
+            arrays[i] = Integer.parseInt(sc.nextLine());
+        }
+        System.out.println("Enter number you want to find: ");
+        int findNumber = Integer.parseInt(sc.nextLine());
+        boolean check = false;
+        int index =0 ;
+        for (int i = 0; i < size; i++) {
+            if (arrays[i] == findNumber){
+                check = true;
+                index = i;
+                break;
+            }
+        }
+        if (check){
+            System.out.println("Yes"+ "--Position:"+ (index+1));
+        } else {
+            System.out.println("No");
         }
     }
 }
