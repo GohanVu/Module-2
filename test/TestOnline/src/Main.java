5b33301094b085af8c35220ec01b12ccd3a79c12
GohanVu <hoangvhdata@gmail.com>
2024-05-27 09:38:09 +0700

    ss14 practice

--- test/TestOnline/src/Main.java
+++ test/TestOnline/src/Main.java
@@ -2,30 +2,24 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Enter large of array: ");
-        Scanner sc = new Scanner(System.in);
-        int size = Integer.parseInt(sc.nextLine());
-        int [] arrays = new int[size];
-        for (int i = 0; i < size; i++) {
-            System.out.println("Enter value "+ (i+1)+": ");
-            arrays[i] = Integer.parseInt(sc.nextLine());
-        }
-        System.out.println("Enter number you want to find: ");
-        int findNumber = Integer.parseInt(sc.nextLine());
-        boolean check = false;
-        int index =0 ;
-        for (int i = 0; i < size; i++) {
-            if (arrays[i] == findNumber){
-                check = true;
-                index = i;
-                break;
+        public static void main(String[] args) {
+            int beerNum = 99;
+            String word = "bottles";
+            while (beerNum > 0){
+                if(beerNum ==1){
+                    word = "bottle";
+                }
+                System.out.println(beerNum+" "+word+" of beer on the wall");
+                System.out.println(beerNum+" "+word+" of beer.");
+                System.out.println("Take one down");
+                System.out.println("Pass it round.");
+                beerNum = beerNum -1;
+                if(beerNum > 0){
+                    System.out.println(beerNum+" "+word+" of beer on the wall");
+                } else {
+                    System.out.println("No more bottles of beer on the wall");
+                }
             }
         }
-        if (check){
-            System.out.println("Yes"+ "--Position:"+ (index+1));
-        } else {
-            System.out.println("No");
-        }
     }
-}
+
