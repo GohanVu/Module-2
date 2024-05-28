0fe3f7f3c9f33986bf33899032e3663b8549176d
GohanVu <hoangvhdata@gmail.com>
2024-05-28 16:05:15 +0700

    update excercise ss15

---
+++ ss15_Exception_Debug/src/exercise/IllegalTriangleException/IllegalTriangleException.java
@@ -0,0 +1,7 @@
+package exercise.IllegalTriangleException;
+
+public class IllegalTriangleException  extends  Exception{
+    public IllegalTriangleException (String message){
+        super(message);
+    }
+}
---
+++ ss15_Exception_Debug/src/exercise/IllegalTriangleException/Main.java
@@ -0,0 +1,16 @@
+package exercise.IllegalTriangleException;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IllegalTriangleException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap canh a");
+        double a = Double.parseDouble(scanner.nextLine());
+        System.out.println("Nhap canh b");
+        double b = Double.parseDouble(scanner.nextLine());
+        System.out.println("Nhap canh c");
+        double c = Double.parseDouble(scanner.nextLine());
+        Triangle triangle = new Triangle(a,b,c);
+    }
+}
