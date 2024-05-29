d3e386a1e11b37d12c1c96e5b2cc955c4eeaa307
GohanVu <hoangvhdata@gmail.com>
2024-05-29 17:09:23 +0700

    update all ss1

---
+++ ss2_Loop/exercise/src/Draw.java
@@ -0,0 +1,16 @@
+public class Draw {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i <= 5 ; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
